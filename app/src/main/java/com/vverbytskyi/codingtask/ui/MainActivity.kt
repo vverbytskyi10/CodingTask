361ad13c7bd07d32c53e903fd1849f41bdd7dce5
Vladyslav <vladyslavverbytskyi@gmail.com>
2019-08-30 23:03:02 +0300

    [add] basic packages and custom Application class

--- app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vverbytskyi.codingtask
+package com.vverbytskyi.codingtask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vverbytskyi.codingtask.R
 
 class MainActivity : AppCompatActivity() {
 
