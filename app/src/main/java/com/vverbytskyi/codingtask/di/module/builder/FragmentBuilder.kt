97f33f75492612af6f21b23e090001474c49c0a8
Vladyslav <vladyslavverbytskyi@gmail.com>
2019-08-31 13:07:59 +0300

    [upd] navigation between list and map

--- app/src/main/java/com/vverbytskyi/codingtask/di/module/builder/FragmentBuilder.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/di/module/builder/FragmentBuilder.kt
@@ -1,6 +1,7 @@
 package com.vverbytskyi.codingtask.di.module.builder
 
-import com.vverbytskyi.codingtask.ui.carslist.CarsListFragment
+import com.vverbytskyi.codingtask.ui.cars.CarsListFragment
+import com.vverbytskyi.codingtask.ui.cars.CarsMapFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,4 +10,7 @@ interface FragmentBuilder {
 
     @ContributesAndroidInjector
     fun contributeCarsListFragment(): CarsListFragment
+
+    @ContributesAndroidInjector
+    fun contributeCarsMapFragment(): CarsMapFragment
 }
--- app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
@@ -1,35 +1,23 @@
 package com.vverbytskyi.codingtask.ui
 
 import android.os.Bundle
-import android.util.Log
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.vverbytskyi.codingtask.R
-import com.vverbytskyi.codingtask.domain.carslist.CarsListUseCase
-import com.vverbytskyi.codingtask.domain.carslist.model.CarsData
-import com.vverbytskyi.codingtask.ui.common.CompletedState
 import dagger.android.support.DaggerAppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
-
-    @Inject
-    lateinit var carsListUseCase: CarsListUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val state = carsListUseCase.getCarsList()
+        initView()
+    }
 
-            if (state is CompletedState<*>) {
-                (state.data as? CarsData)?.also { carsData ->
-                    carsData.cars.forEach { car -> Log.d("car", car.toString()) }
-                }
-            }
-        }
+    private fun initView() {
+        bottomNavView.setupWithNavController(Navigation.findNavController(this, R.id.navHostFragment))
     }
 }
