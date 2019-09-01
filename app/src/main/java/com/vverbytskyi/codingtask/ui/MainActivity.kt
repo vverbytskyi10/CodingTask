720102e070e05b6427a7a804b8fdd64bae1f31db
Vladyslav <vladyslavverbytskyi@gmail.com>
2019-09-01 22:03:06 +0300

    [upd] navigation & icons

--- app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.vverbytskyi.codingtask.ui
 
 import android.os.Bundle
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.NavController
 import com.vverbytskyi.codingtask.R
+import com.vverbytskyi.codingtask.ui.common.extensions.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -15,20 +16,41 @@ class MainActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var factory: ViewModelProvider.Factory
 
+    private var currentNavController: LiveData<NavController>? = null
+
     private lateinit var model: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
 
         model = ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
-
-        setContentView(R.layout.activity_main)
-        initView()
+        model.fetchCars()
     }
 
-    private fun initView() {
-        bottomNavView.setupWithNavController(Navigation.findNavController(this, R.id.navHostFragment))
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
 
-        model.fetchCars()
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(R.navigation.list, R.navigation.map)
+
+        val controller = bottomNavView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.navHostContainer,
+            intent = intent
+        )
+
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
