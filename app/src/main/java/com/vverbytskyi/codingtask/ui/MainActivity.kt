3d1c81d0b355d9a80bdb657427fc48f8f0f1a23a
Vladyslav <vladyslavverbytskyi@gmail.com>
2019-09-01 23:20:33 +0300

    [upd] navigation implementation to ViewPager instead of navigation component

--- app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/ui/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.vverbytskyi.codingtask.ui
 
 import android.os.Bundle
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.NavController
 import com.vverbytskyi.codingtask.R
-import com.vverbytskyi.codingtask.ui.common.extensions.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -16,41 +13,36 @@ class MainActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var factory: ViewModelProvider.Factory
 
-    private var currentNavController: LiveData<NavController>? = null
-
     private lateinit var model: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        }
+        initView()
 
         model = ViewModelProviders.of(this, factory).get(MainViewModel::class.java)
         model.fetchCars()
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-        setupBottomNavigationBar()
-    }
+    private fun initView() {
+        viewPager.apply {
+            offscreenPageLimit = 1
+            isUserInputEnabled = false
+            adapter = CarsFragmentPagerAdapter(this@MainActivity)
+        }
 
-    private fun setupBottomNavigationBar() {
-        val navGraphIds = listOf(R.navigation.list, R.navigation.map)
-
-        val controller = bottomNavView.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.navHostContainer,
-            intent = intent
-        )
-
-        currentNavController = controller
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return currentNavController?.value?.navigateUp() ?: false
+        bottomNavView.setOnNavigationItemSelectedListener { menuItem ->
+            viewPager.currentItem = when (menuItem.itemId) {
+                R.id.menu_item_list -> {
+                    CARS_LIST_INDEX
+                }
+                R.id.menu_item_map -> {
+                    CARS_MAP_INDEX
+                }
+                else -> throw IllegalArgumentException()
+            }
+            true
+        }
     }
 }
