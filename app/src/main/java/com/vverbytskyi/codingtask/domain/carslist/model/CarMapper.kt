d3a54c5f131dc04357b27f8915fc0be3421a7c7e
Vladyslav <vladyslavverbytskyi@gmail.com>
2019-09-01 14:30:26 +0300

    [fix] transmission & fuel types mapping

--- app/src/main/java/com/vverbytskyi/codingtask/domain/carslist/model/CarMapper.kt
+++ app/src/main/java/com/vverbytskyi/codingtask/domain/carslist/model/CarMapper.kt
@@ -4,15 +4,29 @@ import androidx.annotation.VisibleForTesting
 
 typealias DataCarModel = com.vverbytskyi.codingtask.data.network.model.Car
 
+private const val TRANSMISSION_TYPE_A = "A"
+private const val TRANSMISSION_TYPE_M = "M"
+
+private const val FUEL_TYPE_E = "E"
+private const val FUEL_TYPE_D = "D"
+private const val FUEL_TYPE_P = "P"
+
 class CarMapper {
 
     @VisibleForTesting
     private fun dataToDomain(dataModel: DataCarModel): Car {
-        val fuelType = FuelType.values()
-            .find { it.name == dataModel.fuelType } ?: FuelType.UNKNOWN
+        val fuelType = when(dataModel.fuelType) {
+            FUEL_TYPE_E -> FuelType.ETHANOL
+            FUEL_TYPE_D -> FuelType.DIESEL
+            FUEL_TYPE_P -> FuelType.PETROLIUM
+            else -> FuelType.UNKNOWN
+        }
 
-        val transmissionType = TransmissionType.values()
-            .find { it.name == dataModel.transmission } ?: TransmissionType.UNKNOWN
+        val transmissionType = when(dataModel.transmission) {
+            TRANSMISSION_TYPE_A -> TransmissionType.AUTOMATIC
+            TRANSMISSION_TYPE_M -> TransmissionType.MANUAL
+            else -> TransmissionType.UNKNOWN
+        }
 
         val cleanlinessType = CleanlinessType.values()
             .find { it.name == dataModel.innerCleanliness } ?: CleanlinessType.UNKNOWN
